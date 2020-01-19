4c1bf96c0b7422b1ce8bd77ad54be1d28f153047
Farshad Jahanmanesh <farshadjahanmanesh@gmail.com>
2020-01-19 23:48:07 +0330

    Coordinator - adds a starter and navigator coordinator

--- CBA2/CBA2/AppDelegate.swift
+++ CBA2/CBA2/AppDelegate.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
-import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
+	var appCoordinator: AppCoordinator?
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+		self.appCoordinator = AppCoordinator(window: window!)
 		return true
 	}
 
